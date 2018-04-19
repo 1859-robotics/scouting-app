2a982aba664cd49fab6069e052292c6560eb654f
Alexander Griffin <alexander.griffin99@hotmail.com>
2018-04-18 22:46:16 -0400

    )

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -22,16 +22,51 @@ export default class DataTable extends Component {
   }
 
   render() {
+    let tHead, tBody
+    if(this.state.yLabels) {
+      tHead = (
+        <thead>
+          <tr>
+            { this.state.yLabels.map((label, i) => (
+              <td key={ i }>{ label }</td>
+            )) }
+          </tr>
+        </thead>
+      )
+    }
+    if(this.state.data.constructor === Object) {
+      tBody = []
+      Object.keys(this.state.data).forEach((item, i) => {
+        if(!(this.state.data[item].constructor === Array)) {
+          tBody.push(<td key={ tBody.length }>{ this.state.data[item] || "" }</td>)
+        } else {
+          tBody.push(
+            <tr key={ i }>
+              {
+                this.state.xLabels.constructor === Array ? (
+                  this.state.xLabels.map((item, j) => (
+                    <td key={ j }>{ this.state.data[this.state.xLabels[j]] }</td>
+                  ))
+                ) : (
+                  this.state.yLabels.map((label, j) => (
+                    <td key={ j }>{ this.state.data[label] || "" }</td>
+                  ))
+                )
+              }
+            </tr>
+          )
+        }
+      })
+    }
+    if(this.state.xLabels) {
+
+    }
 
     return (
       <table>
-        <thead>
-          <tr>
-
-          </tr>
-        </thead>
+        { tHead || ""}
         <tbody>
-
+          { tBody }
         </tbody>
       </table>
     )
